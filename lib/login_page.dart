e5d6893e7f38ede371e47cee798f391b8c557fec
Amalia181205 <lutvianiamalia@gmail.com>
2026-06-04 20:55:41 +0700

    Fix login_SignIn, sebenernya tahapan sampe sudah berhasil tidak di masukan di github karena baru tau harus di github

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -80,6 +80,7 @@ class _LoginPageState extends State<LoginPage> {
         color: const Color(0xFFF6F6F6),
         borderRadius: BorderRadius.circular(10),
       ),
+
       child: TextField(
         controller: controller,
         obscureText: obscure,
@@ -161,16 +162,6 @@ class _LoginPageState extends State<LoginPage> {
                   obscure: true,
                 ),
 
-                Align(
-                  alignment: Alignment.centerRight,
-                  child: TextButton(
-                    onPressed: () {},
-                    child: const Text(
-                      "Lupa Password?",
-                      style: TextStyle(color: Colors.black),
-                    ),
-                  ),
-                ),
 
                 const SizedBox(height: 10),
 
