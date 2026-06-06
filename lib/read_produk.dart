420520342777250ba7cb80039fc3df629ae627c5
Amalia181205 <lutvianiamalia@gmail.com>
2026-06-06 16:30:42 +0700

    TAMPILAN stock crud terbaru

--- lib/read_produk.dart
+++ lib/read_produk.dart
@@ -72,20 +72,102 @@ class _ReadProdukState extends State<ReadProduk> {
       body: isLoading
           ? const Center(child: CircularProgressIndicator())
           : products.isEmpty
-          ? const Center(child: Text("Belum ada data"))
+          ? const Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(
+                    Icons.inventory_2_outlined,
+                    size: 70,
+                    color: Colors.grey,
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    "Belum ada data produk",
+                    style: TextStyle(fontSize: 18, color: Colors.grey),
+                  ),
+                ],
+              ),
+            )
           : RefreshIndicator(
               onRefresh: refreshData,
               child: ListView.builder(
+                padding: const EdgeInsets.all(16),
                 itemCount: products.length,
                 itemBuilder: (context, index) {
                   final p = products[index];
 
-                  return Card(
-                    margin: const EdgeInsets.all(10),
+                  return Container(
+                    margin: const EdgeInsets.only(bottom: 15),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(18),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withValues(alpha: 0.1),
+                          blurRadius: 8,
+                          offset: const Offset(0, 3),
+                        ),
+                      ],
+                    ),
                     child: ListTile(
-                      title: Text(p.name),
-                      subtitle: Text("Stok: ${p.stock}"),
-                      trailing: Text("Rp ${p.price.toInt()}"),
+                      contentPadding: const EdgeInsets.all(16),
+
+                      leading: CircleAvatar(
+                        radius: 28,
+                        backgroundColor: const Color(
+                          0xff4B2A21,
+                        ).withValues(alpha: 0.1),
+                        child: const Icon(
+                          Icons.inventory_2,
+                          color: Color(0xff4B2A21),
+                        ),
+                      ),
+
+                      title: Text(
+                        p.name,
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 17,
+                        ),
+                      ),
+
+                      subtitle: Padding(
+                        padding: const EdgeInsets.only(top: 8),
+                        child: Row(
+                          children: [
+                            const Icon(
+                              Icons.storage,
+                              size: 16,
+                              color: Colors.grey,
+                            ),
+                            const SizedBox(width: 5),
+                            Text(
+                              "Stok : ${p.stock}",
+                              style: const TextStyle(color: Colors.grey),
+                            ),
+                          ],
+                        ),
+                      ),
+
+                      trailing: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          const Text(
+                            "Harga",
+                            style: TextStyle(fontSize: 12, color: Colors.grey),
+                          ),
+                          Text(
+                            "Rp ${p.price.toInt()}",
+                            style: const TextStyle(
+                              color: Colors.green,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                            ),
+                          ),
+                        ],
+                      ),
 
                       onTap: () {
                         Navigator.push(
