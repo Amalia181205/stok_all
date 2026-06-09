a1c74e997e58fa53dccfbef80979a5f75445de9d
Amalia181205 <lutvianiamalia@gmail.com>
2026-06-09 18:04:46 +0700

    ..

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:provider/provider.dart';
 
 import 'firebase_options.dart';
@@ -8,17 +9,30 @@ import 'providers/auth_provider.dart';
 import 'services/notification_service.dart';
 import 'splash_screen.dart';
 
+@pragma('vm:entry-point')
+Future<void> firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  debugPrint('Background Message: ${message.notification?.title}');
+}
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
+  // Background handler
+  FirebaseMessaging.onBackgroundMessage(firebaseMessagingBackgroundHandler);
+
   // Inisialisasi notifikasi
   await NotificationService.initialize();
 
   // Ambil token FCM (opsional)
-  await NotificationService.getToken();
+  // await NotificationService.getToken();
+
+  // Ambil token untuk pengecekan
+  final token = await NotificationService.getToken();
+  debugPrint("FCM TOKEN: $token");
 
   runApp(
     MultiProvider(
